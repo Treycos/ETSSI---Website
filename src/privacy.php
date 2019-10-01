454996fffc5234e44737c392e7c1c00ff22c82a1
Snowirbix <snowirbix@gmail.com>
2019-10-01 19:38:15 +0200

    Move to PHP

--- src/privacy.php
+++ src/privacy.php
@@ -1,3 +1,52 @@
 <?php header('Content-Type: text/html; charset=utf-8'); ?>
 <?php header('Content-Language: fr'); ?>
-<?php require "privacy.html" ?>
+<!doctype html>
+<html class="no-js" lang="fr">
+
+<head>
+    <title>Exia Conférence - Données Personnelles</title>
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/cesi_favicon.png">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style_minified.css">
+</head>
+
+<body>
+    <?php require "header.html" ?>
+    <div class="about_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-7 col-lg-7">
+                    <div class="about_thumb d-flex">
+                        <div class="img_1">
+                            <img src="img/offers/Privacy.jpeg" alt="">
+                        </div>
+                        <div class="img_2">
+                            <img src="img/about/affiche_conference_optimized.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-lg-5">
+                    <div class="about_info">
+                        <div class="section_title mb-20px">
+                            <span>privacy</span>
+                            <h3>Helping companies understand and apply data
+                                private protection laws.</h3>
+                        </div>
+                        <p>Nowadays, in a world marked by digital transition, the protection and management of personal data has become a priority for our society.
+                            Personal data is information that relates to a natural person, identified directly or indirectly. It can be a name, a photograph, a phone number or an email.
+                            Some data are sensitive as they relate to information that may give rise to discrimination or prejudice.
+                            </p>
+                        <a href="#" class="line-button">Plus de détails</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php require "footer.html" ?>
+</body>
+</html>
